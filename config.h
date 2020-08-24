6652ef3e41c4218b7559a840ea4db78bd532c0a7
Nghia Le Minh <mnrce@protonmail.com>
2020-08-24 22:04:41 +0700

    add my configuration

---
+++ config.h
@@ -0,0 +1,19 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "red",         /* CapsLock on */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
+
+/* ignore empty password when pressing return */
+static const int ignoreempty = 1;
